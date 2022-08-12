cfec7ea87fd48531b2a2ad4971889fbabefd515a
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-12 21:54:09 +0200

    Add routes for ads

---
+++ routes/ads.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const AdController = require("../controllers/ads.controller");
+
+router.get("/ads", AdController.getAllAds);
+router.get("/ads/:id", AdController.getAdById);
+router.post("/ads", AdController.addAd);
+router.put("/ads/:id", AdController.updateAd);
+router.delete("/ads:id", AdController.deleteAd);
+
+module.exports = router;
