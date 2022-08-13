9d8a55603a59e4144c84a89e0578ecb8873373aa
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-13 19:02:15 +0200

    Add app use express urlencoded, change app get for all other req, and add auth routes

--- server.js
+++ server.js
@@ -7,19 +7,21 @@ const app = express();
 
 connectToDB();
 
-const server = app.listen(8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port: 8000");
 });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.use("/api", require("./routes/ads.routes"));
+app.use("/auth", require("./routes/auth.routes"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build"));
 });
 
 app.use((req, res) => {
