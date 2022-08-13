2ea1790ef4979943725be8ebc4a0685bbd52c318
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-13 18:58:43 +0200

    Create endpoints for auth

---
+++ controllers/auth.controller.js
@@ -0,0 +1,32 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+
+    if (
+      login &&
+      typeof login === "string" &&
+      password &&
+      typeof password === "string"
+    ) {
+      const userWithLogin = await User.findOne({ login });
+      if (userWithLogin) {
+        return res.status(409).json({ message: "User already exists" });
+      }
+
+      const user = await User.create({
+        login,
+        password: await bcrypt.hash(password, 10),
+      });
+      res.status(201).send({ message: "User created " + user.login });
+    } else {
+      res.status(400).send({ message: "Bad request" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.login = async (req, res) => {};
