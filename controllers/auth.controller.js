6ec88a5a8e0a67f11ea719edc300ca3dde0b646b
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-13 20:30:38 +0200

    Create endpoint for logged user and add session for login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -40,11 +40,14 @@ exports.login = async (req, res) => {
       typeof password === "string"
     ) {
       const user = await User.findOne({ login });
+
       if (!user) {
         res.status(400).send({ message: "Login or password are incorrect" });
       } else {
         if (bcrypt.compareSync(password, user.password)) {
           res.status(200).send({ message: "Login successful" });
+          req.session.login = user.login;
+          req.session.save();
         } else {
           res.status(400).send({ message: "Login or password are incorrect" });
         }
@@ -52,5 +55,13 @@ exports.login = async (req, res) => {
     }
   } catch (err) {
     res.status(500).json({ message: err });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  if (req.session.login) {
+    res.send({ login: req.session.login });
+  } else {
+    res.status(401).send({ message: "You are not authorized" });
   }
 };
