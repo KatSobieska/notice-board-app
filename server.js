880927caabd42d8610ef780c32158c475d58a3aa
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-13 23:24:46 +0200

    Use var for secret

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: "xyz567",
+    secret: process.env.SECRET_KEY,
     store: MongoStore.create(mongoose.connection),
     resave: false,
     saveUninitialized: false,
