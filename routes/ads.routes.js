babeef1eb96693912ee5ee5d86eb8a846ca4e7c3
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-17 07:15:08 +0200

    Add authMiddleware for endpoints available for logged users

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const AdController = require("../controllers/ads.controller");
+const authMiddleware = require("../utils/authMiddleware");
 
 router.get("/ads", AdController.getAllAds);
 router.get("/ads/:id", AdController.getAdById);
-router.post("/ads", AdController.addAd);
-router.put("/ads/:id", AdController.updateAd);
-router.delete("/ads:id", AdController.deleteAd);
+router.post("/ads", authMiddleware, AdController.addAd);
+router.put("/ads/:id", authMiddleware, AdController.updateAd);
+router.delete("/ads:id", authMiddleware, AdController.deleteAd);
 router.get("/ads/search/:searchPhrase", AdController.searchPhrase);
 
 module.exports = router;
