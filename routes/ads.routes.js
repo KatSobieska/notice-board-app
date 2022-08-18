fb9ff4b7529771fe416fcfc7015cad757dc635f5
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-18 22:45:53 +0200

    Correct router post for adding new Ad

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const AdController = require("../controllers/ads.controller");
 const authMiddleware = require("../utils/authMiddleware");
+const imageUpload = require("../utils/imageUpload.js");
 
 router.get("/ads", AdController.getAllAds);
 router.get("/ads/:id", AdController.getAdById);
-router.post("/ads", authMiddleware, AdController.addAd);
+router.post(
+  "/ads",
+  authMiddleware,
+  imageUpload.single("photo"),
+  AdController.addAd
+);
 router.put("/ads/:id", authMiddleware, AdController.updateAd);
 router.delete("/ads/:id", authMiddleware, AdController.deleteAd);
 router.get("/ads/search/:searchPhrase", AdController.searchPhrase);
