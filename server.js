6d2366be8f8c28d5772041128a387be7118d8c2e
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-18 20:26:34 +0200

    Add app use for dirname public

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/api", require("./routes/ads.routes"));
 app.use("/auth", require("./routes/auth.routes"));
