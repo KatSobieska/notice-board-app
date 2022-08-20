e95037eb3a50fb9ee1eb403f96a9cabb4b9c2c6a
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-20 16:33:38 +0200

    Add Navbar

---
+++ client/src/components/layout/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Navbar
+      bg="primary"
+      variant="dark"
+      expand="lg"
+      className="mt-4 mb-4 rounded"
+    >
+      <Container>
+        <Navbar.Brand>Notice board.app</Navbar.Brand>
+        <Nav className="justiryf-content-end">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
