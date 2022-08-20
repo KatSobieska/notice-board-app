1ce5f7ecf2f85fe950f1f9e2a2211d65eba0d875
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-20 19:43:21 +0200

    Create subreducers and import initialState

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,19 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import adsReducer from "./adsRedux";
+import initialState from "./initialState";
+import usersReducer from "./usersRedux";
 
-import ads from "./adsRedux";
-import users from "./usersRedux";
+const subreducers = {
+  ads: adsReducer,
+  users: usersReducer,
+};
 
-const rootReducer = combineReducers({ ads, users });
+const rootReducer = combineReducers(subreducers);
 
 const store = createStore(
   rootReducer,
+  initialState,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
