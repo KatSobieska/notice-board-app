a8a7984214de91a779e44ea68747d491cfcc5352
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-20 16:33:14 +0200

    Add Footer

---
+++ client/src/components/layout/Footer/Footer.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container className="mt-4 text-muted text-center">
+      Copyright © Notice Board App 2022
+    </Container>
+  );
+};
+
+export default Footer;
