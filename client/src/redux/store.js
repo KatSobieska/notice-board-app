59e50c8776e846f664f0c3d980bb2f28b80bef36
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-20 17:38:41 +0200

    Create store.js

---
+++ client/src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import ads from "./adsRedux";
+import users from "./usersRedux";
+
+const rootReducer = combineReducers({ ads, users });
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
