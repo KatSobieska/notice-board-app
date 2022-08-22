f87a28cbb5d6a142263ac951835f434158b32456
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-22 17:09:36 +0200

    Update DB connection

--- db.js
+++ db.js
@@ -3,7 +3,16 @@ const mongoose = require("mongoose");
 const loadTestData = require("./testData");
 
 const connectToDB = () => {
-  mongoose.connect("mongodb://localhost:27017/noticeBoardDB", {
+  const NODE_ENV = process.env.NODE_ENV;
+  let dbUri = "";
+
+  if (NODE_ENV === "production")
+    dbUri = `mongodb://localhost:27017/noticeBoardDB`;
+  else if (NODE_ENV === "test")
+    dbUri = "mongodb://localhost:27017/noticeBoardDBtest";
+  else dbUri = "mongodb://localhost:27017/noticeBoardDB";
+
+  mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
