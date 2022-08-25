84ec393449d6655dee03ab6e9fce6d5d64765436
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-08-25 18:47:35 +0200

    Add grid styles

--- client/src/components/features/Ads/Ads.js
+++ client/src/components/features/Ads/Ads.js
@@ -2,7 +2,13 @@ import Ad from "../Ad/Ad";
 
 const Ads = ({ ads }) => {
   return (
-    <section>
+    <section
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gridGap: 20,
+      }}
+    >
       {ads.map((ad) => (
         <Ad key={ad._id} {...ad} />
       ))}
