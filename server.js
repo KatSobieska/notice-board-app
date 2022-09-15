f2023aeaf1a74c710437644fdf7fc1ac4a127852
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-15 13:13:13 +0200

    Change app use cors and session

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port: 8000");
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:8000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -23,6 +28,10 @@ app.use(
     store: MongoStore.create(mongoose.connection),
     resave: false,
     saveUninitialized: false,
+    mongoUrl: process.env.DB_URL,
+    cookie: {
+      secure: process.env.NODE_ENV == "production",
+    },
   })
 );
 
