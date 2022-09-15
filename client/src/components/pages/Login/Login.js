991a39a7966fae52be1e8d1ce12ca00ad5e88e59
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-15 13:11:26 +0200

    Add credentials

--- client/src/components/pages/Login/Login.js
+++ client/src/components/pages/Login/Login.js
@@ -16,6 +16,7 @@ const Login = () => {
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
       body: JSON.stringify({ login, password }),
     };
 
