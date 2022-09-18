9adf934824e39401d8ed0a86e1603e99dcd6d045
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-18 10:33:15 +0200

    Add createAd and use credentials

--- client/src/components/features/AdAdd/AdAdd.js
+++ client/src/components/features/AdAdd/AdAdd.js
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../../../config";
-import { addAd } from "../../../redux/adsRedux";
+import { createAd } from "../../../redux/adsRedux";
 import AdForm from "../AdForm/AdForm";
 
 const AdAdd = () => {
@@ -9,19 +9,20 @@ const AdAdd = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (ad) => {
-    dispatch(addAd(ad));
+    dispatch(createAd(ad));
     const fd = new FormData();
     fd.append("title", ad.title);
     fd.append("description", ad.description);
-    fd.append("publicationDate", ad.publicationDate);
-    fd.append("photo", ad.photo);
     fd.append("price", ad.price);
+    fd.append("photo", ad.photo);
     fd.append("location", ad.location);
+    fd.append("publicationDate", ad.publicationDate);
     fd.append("seller", ad.seller);
 
     const options = {
       method: "POST",
       body: fd,
+      credentials: "include",
     };
     fetch(`${API_URL}/api/ads`, options);
     navigate("/");
