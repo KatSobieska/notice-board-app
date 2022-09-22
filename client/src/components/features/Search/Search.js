dd09db1a2c195354caf97555e6d1983cb0dcdd9a
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-22 22:31:05 +0200

    Remove Navigate from imports

--- client/src/components/features/Search/Search.js
+++ client/src/components/features/Search/Search.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { findAdBySearchPhrase, getAllAds } from "../../../redux/adsRedux";
 import Ads from "../Ads/Ads";
 
