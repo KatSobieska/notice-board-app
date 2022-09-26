f8f4a2ab5a106de1b58ff859133fe8f8f0ac7add
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-09-26 20:36:24 +0200

    Add config vars

--- db.js
+++ db.js
@@ -4,10 +4,12 @@ const loadTestData = require("./testData");
 
 const connectToDB = () => {
   const NODE_ENV = process.env.NODE_ENV;
+  const NBA_USERNAME = process.env.NBA_USERNAME;
+  const NBA_PASSWORD = process.env.NBA_PASSWORD;
   let dbUri = "";
 
   if (NODE_ENV === "production")
-    dbUri = `mongodb://localhost:27017/noticeBoardDB`;
+    dbUri = `mongodb+srv://${NBA_USERNAME}:${NBA_PASSWORD}@cluster0.ii8kz.mongodb.net/noticeBoardDB`;
   else if (NODE_ENV === "test")
     dbUri = "mongodb://localhost:27017/noticeBoardDBtest";
   else dbUri = "mongodb://localhost:27017/noticeBoardDB";
